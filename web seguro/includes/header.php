885a2ee245c72b5eead380688eee97d8cab79282
BrennisC <brennis.castro@unas.edu.pe>
2026-07-25 17:27:19 -0500

    feat: corregir la imagenes

--- web seguro/includes/header.php
+++ web seguro/includes/header.php
@@ -3,7 +3,7 @@
 header('X-Frame-Options: DENY');
 header('X-Content-Type-Options: nosniff');
 header('Referrer-Policy: strict-origin-when-cross-origin');
-header("Content-Security-Policy: default-src 'self'; img-src 'self' https://picsum.photos; style-src 'self'; script-src 'self'; base-uri 'self'; frame-ancestors 'none'");
+header("Content-Security-Policy: default-src 'self'; img-src 'self' https://picsum.photos https://fastly.picsum.photos; style-src 'self'; script-src 'self'; base-uri 'self'; frame-ancestors 'none'");
 ?>
 <!doctype html>
 <html lang="en">
